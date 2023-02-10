7bf69ececb6989c581c5993531cb1f82eddd82ab
L-Marina <marina.liemak@gmail.com>
2023-02-10 15:32:40 +0200

    new commit

--- jsIT/js/script.js
+++ jsIT/js/script.js
@@ -1,7 +1,11 @@
-var buttonPlus = document.getElementById("buttonPlus");
-var buttonMinus = document.getElementById("buttonMinus");
-var buttonMultiply = document.getElementById("buttonMultiply");
-var buttonDevide = document.getElementById("buttonDevide");
+// var buttonPlus = document.getElementById("buttonPlus");
+// var buttonMinus = document.getElementById("buttonMinus");
+// var buttonMultiply = document.getElementById("buttonMultiply");
+// var buttonDevide = document.getElementById("buttonDevide");
+
+// var operationButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
+
+var operationButtons = document.getElementsByClassName("operation-button");
 
 var input1 = document.getElementById("number1");
 var input2 = document.getElementById("number2");
@@ -29,8 +33,6 @@ function onOperationButtonClick(eventObject) {
   var operation = clickedElement.innerHTML;
   makeOperations(operation);
 }
-
-var operationButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
 
 for (var i = 0; i < operationButtons.length; i++) {
   operationButtons[i].addEventListener("click", onOperationButtonClick);
