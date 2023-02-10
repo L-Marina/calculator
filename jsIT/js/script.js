f812fc299469ac56e4d9d1d50f474ba7f3062b1a
L-Marina <marina.liemak@gmail.com>
2023-02-10 16:30:12 +0200

    commit

--- jsIT/js/script.js
+++ jsIT/js/script.js
@@ -1,10 +1,3 @@
-// var buttonPlus = document.getElementById("buttonPlus");
-// var buttonMinus = document.getElementById("buttonMinus");
-// var buttonMultiply = document.getElementById("buttonMultiply");
-// var buttonDevide = document.getElementById("buttonDevide");
-
-// var operationButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
-
 var operationButtons = document.getElementsByClassName("operation-button");
 
 var input1 = document.getElementById("number1");
